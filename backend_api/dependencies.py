b9d9f58c90285c962ebc73ef5711631b04f360ab
StephenBrock2 <stephenbrock2@student.purdueglobal.edu>
2026-02-23 14:45:42 -0500

    Simplified dev/prod state change

--- backend_api/dependencies.py
+++ backend_api/dependencies.py
@@ -1,16 +1,22 @@
 from mem_repo import MemUserRepository, MemRecipeRepository, MemIngredientRepository
 #from sql_repo import SQLUserRepository, SQLRecipeRepository, SQLIngredientRepository
 
-# In Memory Testing Repositories 
-user_repo = MemUserRepository()
-recipe_repo = MemRecipeRepository()
-ingredient_repo = MemIngredientRepository()
+# define 'dev'/test state versus 'prod'/live state
+state = 'dev'
 
-# Production Database Repositories 
-#user_repo = SQLUserRepository()
-#recipe_repo = SQLRecipeRepository()
-#ingredient_repo = SQLIngredientRepository()
+def state_change(state):
+    if state == 'dev':
+        user_repo = MemUserRepository()
+        recipe_repo = MemRecipeRepository()
+        ingredient_repo = MemIngredientRepository()
+    if state == 'prod':
+        #user_repo = SQLUserRepository()
+        #recipe_repo = SQLRecipeRepository()
+        #ingredient_repo = SQLIngredientRepository()
+        pass
+    return user_repo, recipe_repo, ingredient_repo
 
+user_repo, recipe_repo, ingredient_repo = state_change(state)
 
 def get_user_repo():
     return user_repo
